e9eda25310a4c8ab74e85bedde9ce4e06b2a7c10
nssean <kenny6224@protonmail.com>
2024-06-29 01:14:47 +0200

    updating files doctrine orm for types

--- src/Infrastructure/Security/Doctrine/Type/PurposeType.php
+++ src/Infrastructure/Security/Doctrine/Type/PurposeType.php
@@ -1,6 +1,6 @@
 <?php
 declare(strict_types=1);
-namespace App\Infrastructure\User\Doctrine\Type\Security;
+namespace App\Infrastructure\Security\Doctrine\Type;
 
 use App\Domain\Security\ValueObject\Purpose;
 use Doctrine\DBAL\Platforms\AbstractPlatform;
