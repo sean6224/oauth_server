36f9f9bdbdb16c2b5918608eb65714eb89155c3f
nssean <kenny6224@protonmail.com>
2024-07-02 13:29:12 +0200

    Refactored user-related operations and event handling across multiple files for improved clarity and functionality:
    1. **UserHandler**:
       - Updated `UserHandler` methods to handle additional domain events:
         - `userPasswordChanged`: New method added to apply `UserPasswordChanged` event, updating user passwords.
         - Documentation and method signatures refined for consistency and clarity.
    
    2. **UserManager**:
       - Enhanced user management methods:
         - `changeEmail`: Updated to accept `User` entity directly, ensuring consistency and simplification.
         - `changePassword`: New method introduced to manage password changes, triggering `UserPasswordChanged` events.

--- src/Domain/User/Entity/EventsHandler/UserHandler.php
+++ src/Domain/User/Entity/EventsHandler/UserHandler.php
@@ -5,30 +5,45 @@ namespace App\Domain\User\Entity\EventsHandler;
 use App\Domain\Shared\Exception\DateTimeException;
 use App\Domain\Shared\ValueObject\DateTime;
 use App\Domain\User\Entity\User;
-use App\Domain\User\Event\Settings\UserEmailChanged;
-use App\Domain\User\Event\UserLogOut;
-use App\Domain\User\Event\UserSignedIn;
 use App\Domain\User\Event\UserWasCreated;
+use App\Domain\User\Event\UserSignedIn;
+use App\Domain\User\Event\UserLogOut;
+use App\Domain\User\Event\Settings\UserEmailChanged;
+use App\Domain\User\Event\Settings\UserPasswordChanged;
 use Webmozart\Assert\Assert;
 
 /**
  * This class is responsible for handling domain events related to user entities.
- * It provides methods to apply specific domain events to `User` entity, such as user creation, sign-in, logout, and email change.
+ * It provides methods to apply specific domain events to the `User`
+ * entity, such as user creation, sign-in, logout, and email change.
  *
  * Methods:
- * - `userWasCreated(UserWasCreated $event, User $user)`: Applies the `UserWasCreated` event, setting UUID, username, credentials, and creation timestamp.
- * - `userSignedIn(UserSignedIn $event, User $user)`: Applies the `UserSignedIn` event, updating user's UUID, email, and login timestamp. Throw `DateTimeException` if there's a date-time-related issue.
- * - `logOut(UserLogOut $event, User $user)`: Applies the `UserLogOut` event, setting the UUID and logging out the user. Displays message indicating the user has logged out.
- * - `userEmailChanged(UserEmailChanged $event, User $user)`: Applies the `UserEmailChanged` event, validating that the new email is different from the current one and updating the email and other related attributes. Use `Webmozart\Assert\Assert` for validation.
+ * - `userWasCreated(UserWasCreated $event, User $user)`: Applies the `UserWasCreated` event,
+ * setting UUID, username, credentials, and creation timestamp.
+ * - `userSignedIn(UserSignedIn $event, User $user)`: Applies the `UserSignedIn` event,
+ * updating user's UUID, email, and login timestamp.
+ * Throw `DateTimeException` if there's a date-time-related issue.
+ * - `logOut(UserLogOut $event, User $user)`: Applies the `UserLogOut` event, setting the UUID and logging out the user.
+ * Displays a message indicating the user has logged out.
+ * - `userEmailChanged(UserEmailChanged $event, User $user)`: Applies the `UserEmailChanged` event,
+ * validating that the new email is different from the current one and updating the email and other related attributes.
+ * Use `Webmozart\Assert\Assert` for validation.
+ * - `userPasswordChanged(UserPasswordChanged $event, User $user)`: Applies the `UserPasswordChanged` event,
+ * updating the user's password and other related attributes.
  *
  * Notes:
  * - Proper handling of these domain events ensures consistency in user management across the system.
- * - The class uses the `DateTime` value object to manage timestamps and relies on assertions to validate business rules.
+ * -
+ * The class uses the `DateTime` value object to manage timestamps and relies on assertions to validate business rules.
  */
 class UserHandler
 {
     /**
-     * @throws DateTimeException
+     * Handles the user creation event.
+     *
+     * @param UserWasCreated $event The event triggered when a user is created.
+     * @param User $user The user entity to apply the event to.
+     * @throws DateTimeException If there is an issue with the date-time handling.
      */
     public function userWasCreated(UserWasCreated $event, User $user): void
     {
@@ -64,6 +79,12 @@ class UserHandler
         );
 
         $user->setEmail($event->email);
+        $user->setUpdatedAt($event->updatedAt);
+    }
+
+    public function userPasswordChanged(UserPasswordChanged $event, User $user): void
+    {
+        $user->setPassword($event->password);
         $user->setUpdatedAt($event->updatedAt);
     }
 }
--- src/Domain/User/Entity/Manager/UserManager.php
+++ src/Domain/User/Entity/Manager/UserManager.php
@@ -8,15 +8,17 @@ use App\Domain\Shared\Exception\DateTimeException;
 use App\Domain\Shared\ValueObject\DateTime;
 use App\Domain\User\Entity\EventsHandler\UserHandler;
 use App\Domain\User\Entity\User;
-use App\Domain\User\Event\Settings\UserEmailChanged;
-use App\Domain\User\Event\UserLogOut;
-use App\Domain\User\Event\UserSignedIn;
 use App\Domain\User\Event\UserWasCreated;
+use App\Domain\User\Event\UserSignedIn;
+use App\Domain\User\Event\UserLogOut;
+use App\Domain\User\Event\Settings\UserEmailChanged;
+use App\Domain\User\Event\Settings\UserPasswordChanged;
 use App\Domain\User\Exception\InvalidCredentialsException;
 use App\Domain\User\Specification\Checker\CustomerEmailUniquenessCheckerInterface;
 use App\Domain\User\Specification\Rule\CustomerEmailMustBeUniqueRule;
 use App\Domain\User\ValueObject\Auth\Credentials;
 use App\Domain\User\ValueObject\Email;
+use App\Domain\User\ValueObject\HashedPassword;
 use App\Domain\User\ValueObject\Username;
 use Ramsey\Uuid\UuidInterface;
 
@@ -31,16 +33,19 @@ use Ramsey\Uuid\UuidInterface;
  * Methods:
  * - `create(UuidInterface $uuid, Username $username, Credentials $credentials,
  * CustomerEmailUniquenessCheckerInterface $customerEmailUniquenessChecker)`:
- * Creates new user and triggers the `UserWasCreated` domain event after checking email's uniqueness.
+ * Creates a new user and triggers the `UserWasCreated` domain event after checking the email's uniqueness.
  * Throw `BusinessRuleValidationException` or `DateTimeException` if there's an issue.
  * - `signIn(User $user, string $plainPassword)`: Signs in the user if the password matches.
  * Throw `InvalidCredentialsException` if the credentials are invalid.
  * Triggers the `UserSignedIn` domain event.
- * - `logOut()`: Logs out the user and triggers the `UserLogOut` domain event.
- * - `changeEmail(Email $email, CustomerEmailUniquenessCheckerInterface $customerEmailUniquenessChecker)`:
+ * - `logOut(User $user)`: Logs out the user and triggers the `UserLogOut` domain event.
+ * - `changeEmail(User $user, Email $email, CustomerEmailUniquenessCheckerInterface $customerEmailUniquenessChecker)`:
  * Changes the user's email after checking its uniqueness.
  * Throw `DateTimeException` if there's a date-time-related issue.
  * Triggers the `UserEmailChanged` domain event.
+ * - `changePassword(User $user, HashedPassword $password)`: Changes the user's password.
+ * Throw `DateTimeException` if there's a date-time-related issue.
+ * Triggers the `UserPasswordChanged` domain event.
  */
 class UserManager
 {
@@ -104,5 +109,19 @@ class UserManager
         $event = new UserEmailChanged($user->getUuid(), $email, DateTime::now());
         $this->addDomainEvent($event);
         $this->userHandler->userEmailChanged($event, $user);
+    }
+
+
+    /**
+     * @throws DateTimeException
+     */
+    public function changePassword(
+        User $user,
+        HashedPassword $password,
+    ): void
+    {
+        $event = new UserPasswordChanged($user->getUuid(), $password, DateTime::now());
+        $this->addDomainEvent($event);
+        $this->userHandler->UserPasswordChanged($event, $user);
     }
 }
