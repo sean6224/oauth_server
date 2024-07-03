5ec323c323559d507f7b6717d5dd67e761c9926e
nssean <kenny6224@protonmail.com>
2024-07-03 03:38:17 +0200

    Added value objects for OAuth2 client attributes

---
+++ src/Domain/Oauth/ValueObject/Active.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+namespace App\Domain\Oauth\ValueObject;
+
+use App\Domain\Shared\ValueObject\BoolValue;
+
+final class Active extends BoolValue
+{
+
+}
---
+++ src/Domain/Oauth/ValueObject/Grants.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+namespace App\Domain\Oauth\ValueObject;
+
+use App\Domain\Shared\ValueObject\ArrayValue;
+
+final class Grants extends ArrayValue
+{
+
+}
